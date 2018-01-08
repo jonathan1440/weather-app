918dc48ca0468c246aecc8a7c7fd4dc98fc8b6b1
Jonathan Ziesmer <jonathan@ziesmer.us>
2018-01-08 13:31:38 -0500

    Normalizing the Values for Display
    -Added plotScale vector to scale graph
    -Added variables to store max value of each axis
    -Added variables to store min value of each axis
    -Adjusted definition of x,y,z vars to calculate the normalized position of the point
    -scaled vecotr used in the instantiation of the dataBall
    -added function FindMaxValue to find the max value in a given column of the plotList dict
    -added function FindMinValue to find the min value in a given column of the plotList dict

--- Assets/Scripts/DataPlotter.cs
+++ Assets/Scripts/DataPlotter.cs
@@ -27,6 +27,9 @@ public class DataPlotter : MonoBehaviour
  
     // Object which will contain instantiated prefabs in hiearchy
     public GameObject PointHolder;
+    
+    // Scale of graph, aka how far in space the maximum points go
+    public float plotScale = 10;
  
     // Use this for initialization
     void Start()
@@ -51,19 +54,29 @@ public class DataPlotter : MonoBehaviour
         xName = columnList[columnX];
         yName = columnList[columnY];
         zName = columnList[columnZ];
+        
+        // Get maximums of each axis
+        float xMax = FindMaxValue(xName);
+        float yMax = FindMaxValue(yName);
+        float zMax = FindMaxValue(zName);
+ 
+        // Get minimums of each axis
+        float xMin = FindMinValue(xName);
+        float yMin = FindMinValue(yName);
+        float zMin = FindMinValue(zName);
  
         //Loop through Pointlist
         for (var i = 0; i < pointList.Count; i++)
         {
-            // Get value in poinList at ith "row", in "column" Name
-            float x = System.Convert.ToSingle(pointList[i][xName]);
-            float y = System.Convert.ToSingle(pointList[i][yName]);
-            float z = System.Convert.ToSingle(pointList[i][zName]);
+            // Get value in poinList at ith "row", in "column" Name, normalize
+            float x = (System.Convert.ToSingle(pointList[i][xName]) - xMin) / (xMax - xMin);
+            float y = (System.Convert.ToSingle(pointList[i][yName]) - yMin) / (yMax - yMin);
+            float z = (System.Convert.ToSingle(pointList[i][zName]) - zMin) / (zMax - zMin);
  
             // Instantiate as gameobject variable so that it can be manipulated within loop
             GameObject dataPoint = Instantiate(
                     PointPrefab,
-                    new Vector3(x, y, z),
+                    new Vector3(x, y, z) * plotScale,
                     Quaternion.identity);
  
             // Make child of PointHolder object, to keep points within container in hierarchy
@@ -75,5 +88,36 @@ public class DataPlotter : MonoBehaviour
             // Assigns name to the prefab
             dataPoint.transform.name = dataPointName;         
         }
+    }
+    
+    private float FindMaxValue(string columnName)
+    {
+        //set initial value to first value in column
+        float maxValue = Convert.ToSingle(pointList[0][columnName]);
+ 
+        //Loop through Dictionary, overwrite existing maxValue if new value is larger
+        for (var i = 0; i < pointList.Count; i++)
+        {
+            if (maxValue < Convert.ToSingle(pointList[i][columnName]))
+                maxValue = Convert.ToSingle(pointList[i][columnName]);
+        }
+ 
+        //Spit out the max value
+        return maxValue;
+    }
+    
+    private float FindMinValue(string columnName)
+    {
+        //set initial value to first value in column
+        float minValue = Convert.ToSingle(pointList[0][columnName]);
+ 
+        //Loop through Dictionary, overwrite existing minValue if new value is smaller
+        for (var i = 0; i < pointList.Count; i++)
+        {
+            if (Convert.ToSingle(pointList[i][columnName]) < minValue)
+                minValue = Convert.ToSingle(pointList[i][columnName]);
+        }
+ 
+        return minValue;
     }
 }
